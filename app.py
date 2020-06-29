47050ca97e96403a226940e73c8bb859131926ed
vishnu <pallivihsnu4444@gmail.com>
2020-06-29 19:43:33 +0530

    updated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-from resources.emp import Profile,Clubdelete,Clubnames,Changepasswordstudent,Editprofile,Studentlogin,Displaypostevents,Login,Adminlog,Requesttoclub,Clubmembers,Addclub,Forgotpassword,Allclubdetails,Changepassword,Addclubmembers
+from resources.emp import Profile,Clubdelete,Clubnames,Mail,Changepasswordstudent,Editprofile,Studentlogin,Displaypostevents,Login,Adminlog,Requesttoclub,Clubmembers,Addclub,Forgotpassword,Allclubdetails,Changepassword,Addclubmembers
 from flask_jwt_extended import JWTManager
 
 app=Flask(__name__)
@@ -24,6 +24,7 @@ api.add_resource(Displaypostevents,'/displaypostevents')
 api.add_resource(Studentlogin,'/slogin')
 api.add_resource(Editprofile,'/editprofile')
 api.add_resource(Changepasswordstudent,'/changepasswordstudent')
+api.add_resource(Mail,'/mail')
 
 @jwt.unauthorized_loader
 def missing_token_callback(error):
